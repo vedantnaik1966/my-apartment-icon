0659594de34e0914b109b614a411bcb30518cf45
vedantnaik1966 <vedantnaik1966@gmail.com>
2026-09-03 22:49:21 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "my-apartment-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./My Appartment.png",
+    "./app.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
